f2a5465aeeffe8fce8599850e2b1a0bc3c5bc8c3
Raspredval <rustKravets@gmail.com>
2025-12-18 10:09:40 +0700

    initial commit from local files

---
+++ include/ConsoleStreams.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "FileStreams.hpp"
+#include "TextIO.hpp"
+
+namespace io {
+    static IFileViewStream
+        std_input(stdin);
+    static OFileViewStream
+        std_output(stdout),
+        std_error(stderr);
+
+    static TextInput
+        cin(std_input);
+    static TextOutput
+        cout(std_output),
+        cerr(std_error);
+}
---
+++ source/tests.cpp
@@ -0,0 +1,5 @@
+#include <ConsoleStreams.hpp>
+
+int main() {
+    io::cout.put("Hello, world!\n");
+}
