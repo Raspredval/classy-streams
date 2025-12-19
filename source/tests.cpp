3d450a5df94f7c1e9cc74c784b83029f21f28b3c
Raspredval <rustKravets@gmail.com>
2025-12-19 11:41:06 +0700

    tested stuff, local sockets work great, IPv4 kinda broken, but somewhat working, done testing for now

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,31 @@
 #include <ConsoleStreams.hpp>
+#include "include/NetworkStreams.hpp"
 
 int main() {
-    io::cout.put("Hello, world!\n");
+    try {
+        io::IPv4::ONetworkClient
+            ip_client;
+        
+        auto
+            optConnection   = ip_client.Connect({"127.0.0.1", "1337"});
+        if (!optConnection)
+            throw std::runtime_error("failed to connect to a server");
+
+        while (optConnection->Good()) {
+            std::string
+                strMessage;
+            io::cout.put("enter message: ");
+            io::cin.get_line(strMessage);
+            io::SerialTextOutput(*optConnection)
+                .put(strMessage);
+            if (strMessage == "/exit")
+                return EXIT_SUCCESS;
+        }
+
+        throw std::runtime_error("failed to send a message\n");
+    }
+    catch (std::exception& err) {
+        io::cerr.fmt("error: {}\n", err.what());
+        return EXIT_FAILURE;
+    }
 }
