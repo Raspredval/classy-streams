4a599849698eee4c21abfd50c456247bef8d36db
Raspredval <rustKravets@gmail.com>
2025-12-25 09:46:21 +0700

    changed the binary io test, to successfully find some bugs

--- source/test_binary_io.cpp
+++ source/test_binary_io.cpp
@@ -5,10 +5,15 @@
 int main() {
     auto
         six_seven   = 67;
+    
     io::IOBufferStream
         buff;
-    io::BinaryIO(buff)
-        .put(six_seven)
+    io::BinaryOutput(buff)
+        .put(six_seven);
+
+    six_seven       = 420;
+
+    io::BinaryInput(buff)
         .go_start()
         .get(six_seven);
     io::cout.fmt("the value: {}\n", six_seven);
